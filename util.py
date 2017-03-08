5cae601006a555dea06a69e951465bab812a4c9f
Giles Miclotte <giles.miclotte@ugent.be>
2017-03-08 18:11:50 +0100

    Update svg header and fix bug

--- util.py
+++ util.py
@@ -561,9 +561,11 @@ def start_partial_svg(SVG):
 	elif SVG.type == 'XMAP':
 		w = SVG.dist / SVG.zoom + SVG.border['left'] + SVG.border['right']
 	h = SVG.height
-	svg = '<svg viewBox=\"0 0 ' + str(w) + ' ' + str(h) + '\"'
-	svg += 'xmlns=\"http://www.w3.org/2000/svg\"'
-	svg += 'xmlns:xlink=\"http://www.w3.org/1999/xlink\"'
+	svg = '<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>'
+	svg += '<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">'
+	svg += '<svg viewBox=\"0 0 ' + str(w) + ' ' + str(h) + '\"'
+	svg += ' xmlns=\"http://www.w3.org/2000/svg\"'
+	svg += ' xmlns:xlink=\"http://www.w3.org/1999/xlink\"'
 	svg += '>\n'
 	svg += '<rect x=\"0\" y=\"0\" width=\"' + str(w) + '\" height=\"' + str(h) + '\" style=' + SVG.background_style + '/>\n'
 	svg += '<g transform=\"translate(0,0)\" id=\"alignmentSVG\">\n'
