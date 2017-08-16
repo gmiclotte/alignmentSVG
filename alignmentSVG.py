11c3671b94b4ef5c6677256b222372837890c4ce
Giles Miclotte <giles.miclotte@ugent.be>
2017-08-16 16:41:05 +0200

    Add indicators for contig ends on the reference

--- alignmentSVG.py
+++ alignmentSVG.py
@@ -50,21 +50,31 @@ def main_SAM(argv=None):
 	if argv == None:
 		argv = sys.argv
 	if len(argv) < 7:
-		print('Usage: alignmentSVG.py SAM reference.fasta ref_id ref_begin_idx ref_distance verbose sorted.sam sam_track_idx track1 reads1.fastq kmer_count1 <track2 reads2.fastq kmer_count2 ...>'\
+		print('Usage: alignmentSVG.py SAM reference.fasta ref_id ref_begin_idx ref_distance verbose <--contig contig_start contig_end> sorted.sam sam_track_idx track1 reads1.fastq kmer_count1 <track2 reads2.fastq kmer_count2 ...>'\
 		 	+ '\n' + 'Use \'-\' if no kmer_count file is provided.'\
 			+ '\n' + 'sam_track_idx is the 0-indexed track that corresponds to the SAM file, use -1 if not applicable.'\
 			+ '\n' + 'verbose is 0 if no reference labels are wanted, 1 if they are.'\
 		)
 		exit()
 	#specify input, can be changed to cl options of course
-	reference_file = argv[2]
-	ref_id = int(argv[3])
+	extra = {}
+	idx = 2
+	reference_file = argv[idx]
+	idx += 1
+	ref_id = int(argv[idx])
 	SVG = SVG_properties(argv[1], int(argv[4]), int(argv[5]), bool(int(argv[6])))
-	sam_file = argv[7]
-	sam_track = int(argv[8])
-	tracks = [argv[i] for i in range(9, len(argv), 3)] # this can be several files, each will create a new alignment track
-	read_files = [argv[i] for i in range(10, len(argv), 3)]
-	SVG.kmer_count = [argv[i] for i in range(11, len(argv), 3)]
+	idx = 7
+	extra['contigs'] = []
+	while argv[idx] == '--contig':
+		extra['contigs'] += [[int(argv[idx + 1]), int(argv[idx + 2])]]
+		idx += 3
+	sam_file = argv[idx]
+	idx += 1
+	extra['sam_track'] = int(argv[idx])
+	idx += 1
+	tracks = [argv[i] for i in range(idx, len(argv), 3)] # this can be several files, each will create a new alignment track
+	read_files = [argv[i] for i in range(idx+1, len(argv), 3)]
+	SVG.kmer_count = [argv[i] for i in range(idx+2, len(argv), 3)]
 	#parse input
 	references = [Sequence(s[0], s[1]) for s in fasta_parse(reference_file)]
 	sam_entries = sam_parse(sam_file)
@@ -76,7 +86,7 @@ def main_SAM(argv=None):
 			sequence = Sequence(s[0], s[1])
 			fasta[sequence.name] = sequence
 		reads += [fasta]
-	print(make_svg(SVG, references[ref_id], sam_entries, reads, tracks, sam_track))
+	print(make_svg(SVG, references[ref_id], sam_entries, reads, tracks, extra))
 
 def main(argv=None):
 	if argv == None:
